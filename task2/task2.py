6307b2269c129e6fc08b80d73e77ea1e21100817
Timur Usmanov <fonuatimur@gmail.com>
2024-06-29 10:53:06 +0700

    wrap into "Solution" class

--- task2/task2.py
+++ task2/task2.py
@@ -1,6 +1,6 @@
 from math import isclose, sqrt
 from math import pow as mpow
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 import sys
 
 class Point():
@@ -14,40 +14,46 @@ class Circle():
         self.center = center
         self.radius = radius
 
-def read_circle_file(file_path: str) -> Tuple[Tuple|float]:
-    with open(file_path, 'r') as f:
-        coords = tuple(map(float, f.readline().strip().split()))
-        radius = float(f.readline().strip())
-    return Circle(Point(*coords), radius=radius)
+class Solution():
+    def __init__(self, fp_to_circle: str, fp_to_points: str):
+        self.filepath_to_circle = fp_to_circle
+        self.filepath_to_points = fp_to_points
 
-def read_points_file(file_path: str) -> List[Tuple[float]]:
-    with open(file_path, 'r') as f:
-        lines = [line.strip().split() for line in f.readlines()]
-    return [Point(float(x), float(y)) for x, y in lines]
-
-def get_point_cirle_intersection(point: Point, circle: Circle, rel_tol: float = 1e-09, abs_tol: float = 0.0) -> int:
-    '''
-    Returns if point intersects, includes or excludes cirlce.
-    :param point: Point that would be checked.
-    :param circle: Circle that would be checked.
-    :param rel_tol: Same as math.isclose parameter.
-    :param abs_tol: Same as math.isclose parameter.
-    :return: Returns one of 3 cases: 0 - point on circle, 1 - point inside the circle, 2 - point outside the circle.
-    '''
-    distance_vector = (point.x - circle.center.x, point.y - circle.center.y)
-    distance = sqrt(mpow(distance_vector[0], 2) + mpow(distance_vector[1], 2))
-
-    if isclose(distance, circle.radius, rel_tol=rel_tol, abs_tol=abs_tol):
-        return 0
+        self.circle = self.__read_circle_file()
+        self.points = self.__read_points_file()
     
-    return (distance > circle.radius) + 1
+    def __read_circle_file(self) -> Tuple[Tuple|float]:
+        with open(self.filepath_to_circle, 'r') as f:
+            coords = tuple(map(float, f.readline().strip().split()))
+            radius = float(f.readline().strip())
+        return Circle(Point(*coords), radius=radius)
+
+    def __read_points_file(self) -> List[Tuple[float]]:
+        with open(self.filepath_to_points, 'r') as f:
+            lines = [line.strip().split() for line in f.readlines()]
+        return [Point(float(x), float(y)) for x, y in lines]
+
+    def get_points_cirle_intersections(self, rel_tol: float = 1e-09, abs_tol: float = 0.0) -> int:
+        '''
+        Returns if point intersects, includes or excludes cirlce.
+        :param rel_tol: Same as math.isclose parameter.
+        :param abs_tol: Same as math.isclose parameter.
+        :return: Returns one of 3 cases: 0 - point on circle, 1 - point inside the circle, 2 - point outside the circle.
+        '''
+        intersection_statuses: List[int] = []
+        for point in self.points:
+            distance_vector = (point.x - self.circle.center.x, point.y - self.circle.center.y)
+            distance = sqrt(mpow(distance_vector[0], 2) + mpow(distance_vector[1], 2))
+
+            if isclose(distance, self.circle.radius, rel_tol=rel_tol, abs_tol=abs_tol):
+                intersection_statuses.append(0)
+                continue
+        
+            intersection_statuses.append((distance > self.circle.radius) + 1)
+        
+        return intersection_statuses
 
 if __name__ == "__main__":
-    file_path_to_circle, file_path_to_points = sys.argv[1:]
-
-    circle = read_circle_file(file_path_to_circle)
-    points = read_points_file(file_path_to_points)
-
-    for point in points:
-        print(get_point_cirle_intersection(point=point, circle=circle))
-    
+    solution = Solution(*sys.argv[1:3])
+    output = solution.get_points_cirle_intersections()
+    print(*output, sep='\n')
